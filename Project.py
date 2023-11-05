2ba2862ef5867ed0c3622b5f0c10d15cc1efd95f
Eli Payton <80854811+wesleypayton@users.noreply.github.com>
2023-11-05 17:33:28 -0600

    Added some comments

--- Project.py
+++ Project.py
@@ -4,9 +4,26 @@
 import sys
 
 def main(csvfile):
-    print(csvfile)
+    # First open file and interpret tree
 
+
+
+    # Now take input for source node
+
+
+
+    # Use Dijkstra's algorithm to find shortest path tree
+    #  and cost of least-cost paths for this node
+
+
+
+    # Now calculate distance vector for each node in the
+    #  network using distance-vector algorithm (Calculate using Bellman-Ford equation)
+
+    
+    
     pass
 
 if __name__ == "__main__":
+    # Take topography file name and pass to main
     main(sys.argv[1])
