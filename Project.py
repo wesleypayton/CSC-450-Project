8a299e815d8ab29ae03768afdd9e9e6fd3c12164
Eli Payton <80854811+wesleypayton@users.noreply.github.com>
2023-11-05 17:25:44 -0600

    added main and csvfile name pass

--- Project.py
+++ Project.py
@@ -1,2 +1,12 @@
 # Names: Elijah Payton, Gregory Whitehurst
 # Description: CSC450 Project
+
+import sys
+
+def main(csvfile):
+    print(csvfile)
+
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv[1])
