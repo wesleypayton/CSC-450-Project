7b054d8cbb24a41f110d468e4996a369355b169c
GregWhite2001 <greggregory95@live.com>
2023-11-05 18:11:06 -0600

    Link values as integers

--- routing.py
+++ routing.py
@@ -5,19 +5,25 @@ from sys import *
 from csv import *
 def main(file):
     # First open file and interpret tree
+    # Note: list[1] = link to u, list[2] = link to v, list[3] = link to w, etc.
     top_file = file
     [u_links, v_links, w_links, x_links, y_links, z_links] = [[],[],[],[],[],[]]
     with open(top_file, newline = '') as csvfile:
         csv_reader = reader(csvfile)
+        first_run = True
         for row in csv_reader:
-            
-            [u_node, v_node, w_node, x_node, y_node, z_node] = [row[1], row[2], row[3], row[4], row[5], row[6]]
-            [u_links.append(u_node), v_links.append(v_node), w_links.append(w_node), x_links.append(x_node), y_links.append(y_node), z_links.append(z_node)]      
+            if first_run == True:
+                [u_node, v_node, w_node, x_node, y_node, z_node] = [row[1], row[2], row[3], row[4], row[5], row[6]]
+                [u_links.append(u_node), v_links.append(v_node), w_links.append(w_node), x_links.append(x_node), y_links.append(y_node), z_links.append(z_node)]
+                first_run = False
+            else:
+                [u_node, v_node, w_node, x_node, y_node, z_node] = [int(row[1]), int(row[2]), int(row[3]), int(row[4]), int(row[5]), int(row[6])]
+                [u_links.append(u_node), v_links.append(v_node), w_links.append(w_node), x_links.append(x_node), y_links.append(y_node), z_links.append(z_node)]      
             
     print([u_links, v_links, w_links, x_links, y_links, z_links])
 
     # Now take input for source node
-
+    
 
 
     # Use Dijkstra's algorithm to find shortest path tree
