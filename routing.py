97ab1330c6a19033c3ec24b3c325b312f7b22b19
GregWhite2001 <greggregory95@live.com>
2023-11-05 18:03:18 -0600

    Input mostly working as desired

--- routing.py
+++ routing.py
@@ -1,12 +1,20 @@
 # Names: Elijah Payton, Gregory Whitehurst
 # Description: CSC450 Project
 
-import sys
-
-def main(csvfile):
+from sys import *
+from csv import *
+def main(file):
     # First open file and interpret tree
-
-
+    top_file = file
+    [u_links, v_links, w_links, x_links, y_links, z_links] = [[],[],[],[],[],[]]
+    with open(top_file, newline = '') as csvfile:
+        csv_reader = reader(csvfile)
+        for row in csv_reader:
+            
+            [u_node, v_node, w_node, x_node, y_node, z_node] = [row[1], row[2], row[3], row[4], row[5], row[6]]
+            [u_links.append(u_node), v_links.append(v_node), w_links.append(w_node), x_links.append(x_node), y_links.append(y_node), z_links.append(z_node)]      
+            
+    print([u_links, v_links, w_links, x_links, y_links, z_links])
 
     # Now take input for source node
 
@@ -26,4 +34,4 @@ def main(csvfile):
 
 if __name__ == "__main__":
     # Take topography file name and pass to main
-    main(sys.argv[1])
+    main(argv[1])
